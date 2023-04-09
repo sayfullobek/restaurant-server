95b2ed192ba6d3ce226a92d36b2d9dd381ac89ea
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-04-09 13:42:26 +0300

    Update ZakazRepository.java

--- src/main/java/it/revo/restaranserver/repository/ZakazRepository.java
+++ src/main/java/it/revo/restaranserver/repository/ZakazRepository.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin("http://127.0.0.1:5173")
+@CrossOrigin("https://restaurant-web-client.vercel.app")
 public interface ZakazRepository extends JpaRepository<Zakaz, UUID> {
 }
