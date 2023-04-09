1ea6d9a9d40f120c9f0a09777564951e74b43d52
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-04-09 13:41:53 +0300

    Update ProductRepository.java

--- src/main/java/it/revo/restaranserver/repository/ProductRepository.java
+++ src/main/java/it/revo/restaranserver/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin("http://127.0.0.1:5173")
+@CrossOrigin("https://restaurant-web-client.vercel.app")
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     boolean existsProductByNameUzEqualsIgnoreCaseAndNameEnEqualsIgnoreCaseAndNameRuEqualsIgnoreCase(String nameUz, String nameEn, String nameRu);
 
